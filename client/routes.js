8a2fae284f3ef640c191db236e0c9b189235a3c2
Sam Li <samlzx96@gmail.com>
2019-08-01 14:14:30 -0400

    singleProduct

--- client/routes.js
+++ client/routes.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types'
 import {Login, Signup, UserHome} from './components'
 import {me} from './store'
 import ProductList from './components/ProductList'
+import SingleProduct from './components/SingleProduct'
 
 /**
  * COMPONENT
@@ -20,9 +21,10 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/signup" component={Signup} />
-        <Route path="/" component={ProductList} />
+        <Route exact path="/" component={ProductList} />
+        <Route path="/products/:productId" component={SingleProduct} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
       </Switch>
     )
   }
