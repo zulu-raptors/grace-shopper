2ffc5c809ff8b9876e4670c8215e4a2e78453a09
Ally S <ally.lobova@gmail.com>
2019-08-01 21:47:02 -0400

    added router security

--- server/api/users.js
+++ server/api/users.js
@@ -1,14 +1,15 @@
 const router = require('express').Router()
 const {User, Order} = require('../db/models')
+const {isLoggedIn, isAdmin, isUserOrAdmin} = require('./gatekeeper')
 module.exports = router
 
-router.get('/', async (req, res, next) => {
+router.get('/', isAdmin, async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and email fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      //attributes: ['id', 'email'],
+      attributes: ['id', 'email', 'isAdmin', 'firstName', 'lastName'],
       include: [Order]
     })
     res.json(users)
@@ -17,13 +18,56 @@ router.get('/', async (req, res, next) => {
   }
 })
 
-router.get('/:id', async (req, res, next) => {
+router.get('/:id', isUserOrAdmin, async (req, res, next) => {
   try {
     const user = await User.findByPk(req.params.id, {
       include: [Order]
     })
-    res.json(user)
+    res.json({
+      id: user.id,
+      isAdmin: user.isAdmin,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email
+    })
   } catch (err) {
     next(err)
+  }
+})
+
+//to get a specific users orders
+router.get('/:id/orders', isUserOrAdmin, async (req, res, next) => {
+  try {
+    const userId = req.params.id
+    const orders = await Order.findAll({
+      where: {
+        userId
+      }
+    })
+    res.json(orders)
+  } catch (err) {
+    next(err)
+  }
+})
+
+//to use for our sign up form
+
+router.post('/', async (req, res, next) => {
+  try {
+    await User.create(req.body)
+  } catch (error) {
+    next(error)
+  }
+})
+
+//route for creating new order
+router.post('/:id/orders', isUserOrAdmin, async (req, res, next) => {
+  try {
+    const newOrder = req.body
+    newOrder.userId = req.params.id
+    const order = await Order.create(newOrder)
+    res.json(order)
+  } catch (error) {
+    next(error)
   }
 })
