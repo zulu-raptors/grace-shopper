0c0ad35b53ccc061f8d1989edda77b7ce0486d91
Sam Li <samlzx96@gmail.com>
2019-08-05 16:39:56 -0400

    selector on all product page

--- client/components/EachCart.js
+++ client/components/EachCart.js
@@ -7,7 +7,6 @@ class EachCart extends Component {
     this.state = {
       quantity: 0
     }
-    // this.handleChange = this.handleChange.bind(this)
     this.handleClick = this.handleClick.bind(this)
     this.handleSelect = this.handleSelect.bind(this)
   }
@@ -15,13 +14,6 @@ class EachCart extends Component {
   handleClick(event) {
     this.props.updateCart(this.props.order.id, this.state.quantity)
   }
-
-  // async handleChange(event) {
-  //   await this.setState({
-  //     quantity: event.target.value
-  //   })
-  //   console.log(this.state)
-  // }
 
   async handleSelect(event) {
     event.preventDefault()
@@ -40,15 +32,6 @@ class EachCart extends Component {
         <p>quantity: {order.quantity}</p>
         <p>total: ${order.quantity * order.price / 100}</p>
         <div>
-          {/* <form>
-            <label htmlFor="quantity">quantity:</label>
-            <input
-              type="number"
-              placeholder={order.quantity}
-              value={this.state.quantity}
-              onChange={this.handleChange}
-            />
-          </form> */}
           <select onChange={this.handleSelect}>
             <option>-Quantity-</option>
             {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(num => (
