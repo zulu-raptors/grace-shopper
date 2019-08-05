8847d17cd272c3e33edbdc958c05dff70c68a080
Sam Li <samlzx96@gmail.com>
2019-08-05 15:42:31 -0400

    selector for quantity in  Cartpage

--- client/components/EachCart.js
+++ client/components/EachCart.js
@@ -5,20 +5,27 @@ class EachCart extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      quantity: ''
+      quantity: 0
     }
-    this.handleChange = this.handleChange.bind(this)
+    // this.handleChange = this.handleChange.bind(this)
     this.handleClick = this.handleClick.bind(this)
+    this.handleSelect = this.handleSelect.bind(this)
   }
 
   handleClick(event) {
     this.props.updateCart(this.props.order.id, this.state.quantity)
   }
 
-  async handleChange(event) {
-    await this.setState({
-      quantity: event.target.value
-    })
+  // async handleChange(event) {
+  //   await this.setState({
+  //     quantity: event.target.value
+  //   })
+  //   console.log(this.state)
+  // }
+
+  async handleSelect(event) {
+    event.preventDefault()
+    await this.setState({quantity: event.target.value})
     console.log(this.state)
   }
 
@@ -33,7 +40,7 @@ class EachCart extends Component {
         <p>quantity: {order.quantity}</p>
         <p>total: ${order.quantity * order.price / 100}</p>
         <div>
-          <form>
+          {/* <form>
             <label htmlFor="quantity">quantity:</label>
             <input
               type="number"
@@ -41,7 +48,15 @@ class EachCart extends Component {
               value={this.state.quantity}
               onChange={this.handleChange}
             />
-          </form>
+          </form> */}
+          <select onChange={this.handleSelect}>
+            <option>-Quantity-</option>
+            {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(num => (
+              <option key={num} value={num}>
+                {num}
+              </option>
+            ))}
+          </select>
           <button type="submit" onClick={this.handleClick}>
             update
           </button>
